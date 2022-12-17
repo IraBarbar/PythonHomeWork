c0181e8e873911ebf10640e499ba9ba45ed2de93
IraBarbar <bro998877@yahoo.com>
2022-12-17 14:31:43 +0300

    HomeWork02.finish

--- HomeWork02/Task03.py
+++ HomeWork02/Task03.py
@@ -2,16 +2,16 @@
 # НЕ ИСПОЛЬЗОВАТЬ ВСТРОЕННЫЕ БИБЛИОТЕКИ SHUFFLE, 
 # максимум использование библиотеки Random для и получения случайного int
 
-import random
 from random import randint
 no_mix = range(20)
+
 print(list(no_mix))
 
 mix_list = []
 for i in no_mix:
-    i = random.choice(no_mix)
+    i = randint(0,len(no_mix)-1)
     while len(no_mix) != len(mix_list):
-        i = random.choice(no_mix)
+        i = randint(0,len(no_mix)-1)
         if i not in mix_list:
             mix_list.append(i)
         else:
