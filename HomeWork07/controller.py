2ed0fe2fdf582c59204806e90cf3abe3bcfbcccd
IraBarbar <bro998877@yahoo.com>
2023-01-24 17:53:07 +0300

    Task07.TelContacts

--- HomeWork07/controller.py
+++ HomeWork07/controller.py
@@ -18,6 +18,17 @@ def menu():
 
 list_menu = menu()
 
+def find_contact(list_bd):
+    string_find = str(input('Поиск: '))
+    clause = False
+    for line in list_bd:
+        for val in line.values():
+            if string_find == val :
+                print(line)
+                clause = True      
+    if clause == False:
+        print(f'\tНичего не нашёл.\n')
+        
 def processing_request(num):
     match num:
         case 1:
@@ -26,9 +37,20 @@ def processing_request(num):
         case 2:
             model.set_bd(bd_list)
             processing_request(view.show_menu(list_menu))
+        case 3:
+            model.change_contact()
+            processing_request(view.show_menu(list_menu))
+        case 4:
+            model.del_contact(bd_list)
+            processing_request(view.show_menu(list_menu))
+        case 5:
+            find_contact(bd_list)
+            processing_request(view.show_menu(list_menu))
         case 6: 
             model.save_list(bd_list)
             processing_request(view.show_menu(list_menu))
+        case 7:
+            exit()
 
 
 
--- HomeWork07/view.py
+++ HomeWork07/view.py
@@ -1,17 +1,22 @@
 
 
 def show_all_list(dict_list: list):
-    for i in range(len(dict_list)) :
-        print( *dict_list[i].values())
+    for contact in dict_list  :
+        print( contact)  
+        
         
 
 def show_menu(list_menu):
     print(f'\tМеню:')
     for num, paragraph in enumerate(list_menu) :
         print( f'\t{num+1} - {paragraph}')
-    num_paragraph = int(input('Выберите пункт меню: '))
-    print()
+    num_paragraph =int(input('Выберите пункт меню: '))
+    while (num_paragraph not in range(1,9)):
+        num_paragraph = int(input('Ошибка! Выберите пункт меню: '))
     return num_paragraph     
+    
+        
+ 
 
     
 
